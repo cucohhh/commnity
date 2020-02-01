a9c67bedd03db661c7e3db85b2667b5521394c64
cucohhh <2412504688@qq.com>
2020-02-01 18:29:03 +0800

    thymeleaf used in index  and session

--- src/main/java/com/life/demo/Controller/AuthorizeController.java
+++ src/main/java/com/life/demo/Controller/AuthorizeController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class AuthorizeController {
     @Autowired
@@ -22,7 +24,8 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String callBack(@RequestParam(name="code") String code,
-                           @RequestParam(name="state") String state){
+                           @RequestParam(name="state") String state,
+                           HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientID);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -32,8 +35,17 @@ public class AuthorizeController {
 
         String accessToken  = gitHubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = gitHubProvider.getGitHubUser(accessToken);
-        System.out.println(user.getName());
+        if(user !=null){
+            //登陆成功 操作
+            //写入session
+            request.getSession().setAttribute("user" ,user);
+            return "redirect:/";
+        }else{
+            //登陆失败操作
+            return "redirect:/";
+        }
 
-        return "index";
+
+
     }
 }
